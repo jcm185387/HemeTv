a7aa9fd1fc0972297a02fa7c9074313916dc0292
Juan Cruz Mogica <juan.cruz@neoris.com>
2020-03-24 12:39:54 -0600

    first commit

---
+++ app/src/main/java/com/inovateka/hemetv/splash_Activity.java
@@ -0,0 +1,38 @@
+package com.inovateka.hemetv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splash_Activity extends AppCompatActivity {
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_);
+    }
+    */
+    private final int DURACION_SPLASH = 3000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent( splash_Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
